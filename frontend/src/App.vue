8418fca187cd94b6cd0186ecb178c9d7e1af4a8e
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-07-12 08:10:24 +0900

    Replace with Composition API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,53 +2,59 @@
   <div id="app">
     <h1>Todo App</h1>
     <form @submit.prevent>
-      <input type="text" v-model="title">
+      <input type="text" v-model="state.title">
       <button @click="handleCreateTodo">Add</button>
     </form>
-    <p v-for="(todo, index) in todos" v-bind:key="todo.id">
-      {{ todo.title }} <button @click="handleDeleteTodo(todo.id, index)">Delete</button>
+    <p v-for="todo in state.todos" v-bind:key="todo.id">
+      {{ todo.title }} <button @click="handleDeleteTodo(todo.id)">Delete</button>
     </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue' // defineComponent関数（Vue3でTypeScriptを取り入れる場合に必要）を読み込む
+import { defineComponent, reactive, onMounted } from 'vue' // defineComponent関数（TypeScriptを取り入れる場合に必要）などを読み込む
 import { getTodos, createTodo, deleteTodo } from './lib/api/todos' // APIを呼び出すための関数を読み込む
 import { Todo } from './interfaces/index' // 型定義を読み込む
 
+interface State {
+  todos: Todo[]
+  title: string
+}
+
 export default defineComponent({
-  name: 'App',
-  data: () => ({
-    todos: [] as Todo[], // todos: Todo型の配列
-    title: ''
-  }),
-  methods: {
+  setup() {
+    const state = reactive<State>({
+      todos: [],
+      title: ''
+    })
+
     // Todo一覧を取得
-    handleGetTodos: async function(): Promise<void> {
+    const handleGetTodos = async (): Promise<void> => {
       try {
         const res = await getTodos()
 
         if (res?.status === 200) {
-          this.todos = res.data.todos // todos（配列）に結果を格納
+          state.todos = res.data.todos // todos（配列）に結果を格納
         }
       } catch (err) {
         console.log(err)
       }
-    },
+    }
+
     // Todoの新規作成
-    handleCreateTodo: async function(): Promise<void> {
-      if (this.title === '') return // 入力が空だった場合は以降の処理を行わない
+    const handleCreateTodo = async (): Promise<void> => {
+      if (state.title === '') return // 入力が空だった場合は以降の処理を行わない
 
       // Todo型に沿ったデータを作成
       const data: Todo = {
-        title: this.title
+        title: state.title
       }
 
       try {
         const res = await createTodo(data)
 
         if (res?.status === 200) {
-          this.todos.push(res.data.todo) // todos（配列）の最後尾にtodoを追加
+          state.todos = [...state.todos, res.data.todo] // todos（配列）の最後尾にtodoを追加
         } else {
           console.log(res.data.message)
         }
@@ -56,17 +62,18 @@ export default defineComponent({
         console.log(err)
       }
 
-      this.title = '' // 追加後はtitleを空にする（フォーム内がリセットされる）
-    },
+      state.title = '' // 追加後はtitleを空にする（フォーム内がリセットされる）
+    }
+
     // Todoの削除
-    handleDeleteTodo: async function(id: number, index: number): Promise<void> {
+    const handleDeleteTodo = async (id: number): Promise<void> => {
       if (id == null) return // idが無かった場合は以降の処理を行わない
       
       try {
         const res = await deleteTodo(id)
 
         if (res?.status === 200) {
-          this.todos.splice(index, 1) // インデックス（index）に位置する要素を1つ削除
+          state.todos = state.todos.filter(todo => todo.id !== id) // idが一致しないもののみに絞り込む（要するにidが一致したものを削除する）
         } else {
           console.log(res.data.message)
         }
@@ -74,9 +81,18 @@ export default defineComponent({
         console.log(err)
       }
     }
-  },
-  mounted: function() {
-    this.handleGetTodos() // Vueインスタンスがマウントされるたびに実行される関数
+
+    // Vueインスタンスがマウントされるたびに実行される関数
+    onMounted(() => {
+      handleGetTodos()
+    })
+
+    // template内で使用したいものをreturn
+    return {
+      state,
+      handleCreateTodo,
+      handleDeleteTodo
+    }
   }
 })
 </script>
